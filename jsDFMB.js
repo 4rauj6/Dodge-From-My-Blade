145122c6bd28f540c010967449bb67ef8b407b67
João Victor <159325010+4rauj6@users.noreply.github.com>
2025-08-26 16:17:18 -0300

    Add files via upload

---
+++ jsDFMB.js
@@ -0,0 +1,15 @@
+document.addEventListener("keydown", function(event) {
+    if(event.code === "Space"){
+        event.preventDefault();
+
+        document.querySelector(".nomeTela").style.display = "none";
+        document.querySelector(".boardJogo").style.display = "block";
+        document.querySelector(".space2start").style.display = "none";
+    }
+});
+
+document.addEventListener("keypress", function(event) {
+    if(event.code === "A"){
+        d
+    }
+})
